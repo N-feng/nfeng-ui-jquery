461bdce22c007f419f0f6d7ad4973780a8672bd7
聂杞锋 <308561157@qq.com>
2018-02-09 16:28:47 +0800

    解决冲突

--- static/js/main.js
+++ static/js/main.js
@@ -5,16 +5,10 @@ require.config({
 });
 
 require([
-<<<<<<< HEAD:static/main.js
-    '../router/index',
-    // 'text!../view/layouts/header.html',
-    // 'text!../view/layouts/sidebar.html'
-=======
     './router/index',
     'text!./view/layouts/header.html',
     'text!./view/layouts/sidebar.html'
->>>>>>> gh-pages:static/js/main.js
 ], function (router, header, sidebar) {
-    // $('.header').html(header);
-    // $('.sidebar').html(sidebar);
+    $('.header').html(header);
+    $('.sidebar').html(sidebar);
 });
