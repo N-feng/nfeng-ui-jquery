8d894547ea0d27e4c14f47fadb29adb1781ee34b
聂杞锋 <308561157@qq.com>
2018-02-27 09:59:27 +0800

    font try path

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   output: {
     path: __dirname + '/dist',
     filename: 'js/[name].js',
-    publicPath: '/static/'
+    publicPath: '/dist/'
   },
   plugins: [
     // new webpack.optimize.UglifyJsPlugin({
