afa380b4dcdee777ccf1ef6bb774f53712466e5f
聂杞锋 <308561157@qq.com>
2018-05-04 17:06:16 +0800

    security

--- assets/entry.pc.js
+++ assets/entry.pc.js
@@ -9,7 +9,8 @@ $.each([
     require("./js/base/pubsub"),			// 订阅发布
     require("./js/base/urlHelper"),			// 拿url参数
     require("./js/component/loading"),		// 加载
-    require("./js/component/message"),		// 提示
+    require("./js/component/message"),		// 信息
+    require('./js/component/alert'),        // 提示
     require("./js/component/tooltip"),		// tooltip
 	require("./js/other/clock"),			// 时钟
 	require("./js/other/parallax"),			// 3d视差
